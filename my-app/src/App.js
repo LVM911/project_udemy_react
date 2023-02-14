fd395dfe24c6e996c0c6b9b3b38a2ab668827d09
Volodymyr Lytvyniuk <lvm911@gmail.com>
2023-02-14 12:07:55 +0200

    Ex 123 - Elements and components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import {Component} from 'react';
 import './App.css';
+
+// base rules creating components
+
+const Header = () => {
+  return <h2> Hello world!</h2>
+}
+/* const Feild = () => {
+  const holder = 'Enter here';
+  const styleField = {
+    width: '300px' 
+  };
+  return <input 
+          placeholder={holder} 
+          type="text"
+          style={styleField}/>
+} */
+// class-based approach inheritance (наследование) extends
+class Feild extends Component {
+  render() {
+      const holder = 'Enter here';
+      const styleField = {
+        width: '400px' 
+      };
+      return <input 
+      placeholder={holder} 
+      type="text"
+      style={styleField}/>
+  }
+
+}
+
+
+function Btn() {
+  const text = 'Log in';
+  const logged = false;
+  // function transfer
+/*   const res = () => {
+    return 'Log in here';
+  } */
+  // or another elements
+  /* const p = <p> Log in</p>; */
+  // or logical
+  return <button>{logged ? 'Enter': text}</button>  //ternar operator  - logged ? 'Enter': text
+ 
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Feild/>
+      <Btn/>
     </div>
   );
 }
 
+export {Header};
 export default App;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,22 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {Header} from './App';
 
 
-const text = "Hello world";
-// all thinks without objects
-
-const elem = (
-  <div>
-    <h2 className='Text'   >Text: {text} = {2*9}  </h2>
-    <input type="text" />
-    <label htmlFor=''> Label</label>
-    <button tabIndex={0} > ckick </button>
-  </div>
-);
 
 ReactDOM.render(
-  elem,
+ 
+  <App/>,
   document.getElementById('root')
 );
 
